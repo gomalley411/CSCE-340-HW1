71c55d1e34462156aef21bd0b8e85ff6dc3af0bb
wachelmac <78569827+wachelmac@users.noreply.github.com>
2021-02-10 14:11:26 -0600

    Upload PercolationStats.java
    The code I said I'd write

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,52 +1,98 @@
+//Rachel McMullan
+//George O'Malley 
+
+import java.util.Random;
+import java.lang.Math;
 
 public class PercolationStats {
-	/**
-	 * perform t independent experiments on an N-by-N grid
-	 * @param n -- size of grid
-	 * @param t -- number of independent experiments
-	 */
-	public PercolationStats(int n, int t) {
-		if (n <= 0 || t <= 0) throw new IllegalArgumentException();
-		/* take two arguments N and T to perform experiments.
-		 * calculate the mean, standard deviation, and both ends of the
-		 * 95% confidence interval for perc. threshold.
-		 * 
-		 * USE A Random CLASS TO GENERATE RANDOM NUMBERS
-		 * 
-		 * DESIGN AN ALGORITHM TO COMPUTE THE SAMPLE MEAN AND STANDARD DEVIATIOn
-		 */
-	}
-	
-	/**
-	 * I will design an algorithm to compute the mean
-	 * @return the sample mean of percolation threshold
-	 */
-	public double mean() {
-		// write later
-	}
-	
-	/**
-	 * I will design an algorithm to compute the standard deviation
-	 * @return sample standard deviation of percolation threshold
-	 */
-	public double stddev() {
-		// write later
-		
-	}
-	
-	/**
-	 * 
-	 * @return low endpoint of 95% confidence interval
-	 */
-	public double confidenceLow() {
-		// write later
-	}
-	
-	/**
-	 * 
-	 * @return high endpoint of 95% confidence interval
-	 */
-	public double confidenceHigh() {
-		// write later
-	}
+    
+    private int size, trials;   //grid dimensions and number of experiments
+    private double[] thresholds;//percolation thresholds for each trial
+    private Random random;
+    private Percolation grid;   //current system
+    
+    //perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T){
+        
+        //validate arguments
+        if(N<=0){
+            throw new IllegalArgumentException(Integer.toString(N));}
+        if(T<=0){
+            throw new IllegalArgumentException(Integer.toString(T));}
+        
+        //initialize global fields 
+        size=N; 
+        trials=T;
+        thresholds=new double[T];
+        
+        int count;  //number of sites opened before percolation 
+        
+        //loop repeats the experiment T times 
+        for(int t=0; t<T; t++){
+            
+            //reset upon each iteration 
+            count=0; 
+            grid=new Percolation(size);
+            
+            //loop repeats until system percolates
+            while(!grid.percolates()){
+                
+                System.out.println(count);
+                
+                //open a site at a random index
+                getOpen(random.nextInt(size), random.nextInt(size));
+                    
+                count++; //update number of open sites
+            }
+            
+            //calculate/store percolation threshold of the current experiment 
+            thresholds[t]= count/(double)(size*size);
+        }
+    }
+    
+    //open a blocked site at a random index
+    private boolean getOpen(int i, int j){
+        
+        //base case: site is blocked
+        if(!grid.isOpen(i, j)){
+            grid.open(i, j);
+            System.out.println(i+" "+j);
+            return true;
+        }
+        //recursive case: site at index is open, generate another index
+        else{
+            return getOpen(random.nextInt(size), random.nextInt(size));   
+        }
+    }
+    
+    //sample mean of percolation threshold
+    public double mean(){ 
+        
+        int total=0; 
+        for(int t=0; t<trials; t++){
+            total+=thresholds[t];
+        }
+        return total/trials;
+    }
+    
+    //sample standard deviation of percolation threshold
+    public double stddev(){
+        
+        double sum=0; 
+        for(int t=0; t<trials; t++){
+            sum += Math.pow(thresholds[t]-mean(), 2);
+        }
+        return Math.sqrt(sum/(trials-1));
+    }
+    
+    //low  endpoint of 95% confidence interval
+    public double confidenceLow(){
+        
+        return mean()-(1.96 * stddev()/Math.sqrt(trials));
+    }
+    
+    //high endpoint of 95% confidence interval
+    public double confidenceHigh(){
+        return mean()+(1.96 * stddev()/Math.sqrt(trials));
+    }
 }
