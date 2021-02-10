aa13f33518f95b8652246bd0481afe0d5c21ec2b
wachelmac <78569827+wachelmac@users.noreply.github.com>
2021-02-10 14:23:19 -0600

    Add edits

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,123 +1,88 @@
-public class Percolation {
-	// refer to textbook or to Prof. Wu 
-	// if help is needed, but do NOT copy
-
-	
-	private boolean[][] sites;
-	private int mySize;
-	private WeightedQuickUnionUF myPerc;
-	private WeightedQuickUnionUF myFull;
-	private int myTop, myBottom;
-	
-	public Percolation(int size) { // create size by size grid, with all sites blocked
-		if (size <= 0) throw new IllegalArgumentException();
-		sites = new boolean[size][size];
-		mySize = size;
-		myPerc = new WeightedQuickUnionUF(mySize * mySize + 2);
-		myFull = myPerc;
-		myTop = mySize * mySize;
-		myBottom = myTop + size;
-		
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				sites[i][j] = false;
-			}
+public class Percolation{
+    
+    private boolean sites[][];
+    private int mySize;
+    private WeightedQuickUnionUF myPerc;
+    private WeightedQuickUnionUF myFull;
+    private int myTop, myBottom; 
+    
+    public Percolation(int size){ // create size by size grid, with all sites blocked
+        
+        if(size<=0) throw new IllegalArgumentException();
+        
+        sites=new boolean[size][size];
+        mySize=size; 
+        myPerc=new WeightedQuickUnionUF(mySize * mySize + 2);
+        myFull=myPerc; 
+        myTop=mySize*mySize;
+        myBottom=myTop+mySize;
+        
+        for(int i=0; i<size; i++){
+            for(int j=0; j<size; j++){
+                sites[i][j]=false;
+            }
+        }
+    }
+    
+    private int getIndex(int i, int j){
+        
+        if(i<0 || i>=mySize || j<0 ||j >=mySize) 
+            throw new IndexOutOfBoundsException();
+        
+        return i*mySize+j;
+    }
+    
+    public void open(int i, int j){
+        validate(i, j);
+        if(!sites[i][j])
+            sites[i][j]=true; 
+        
+        int index=getIndex(i,j);
+        
+        if(i==0){
+            myPerc.union(myTop, index);
+            myFull.union(myTop, index);
+        }
+        if(i==mySize-1) 
+            myPerc.union(myBottom, index);
+        
+    }
+    
+    //returns truth value of the given site, where true is open
+    public boolean isOpen(int i, int j){
+        if(i<0 || j<0 || i>=mySize || j>=mySize)
+            return false; //I still think it should be IndexOutOfBoundsException
+        return sites[i][j];
+    }
+    
+    public boolean isFull(int i, int j){
+        validate(i, j);
+        return myFull.connected(myTop, getIndex(i, j));
+    }
+    
+    public void validate(int i, int j) {
+	if (i < 0 || i >= mySize) {
+            throw new IllegalArgumentException("open(): index " + i + " is not between 0 and " + (mySize-1));
+	}
+	else if (j < 0 || j >= mySize) {
+            throw new IllegalArgumentException("open(): index " + j + " is not between 0 and " + (mySize-1));
+	}
+    }
+    
+    public void print() {
+	for (int i = 0; i < mySize; i++) {
+            for (int j = 0; j < mySize; j++) {
+		if (sites[i][j]) 
+                    System.out.print("O");
+		else 
+                    System.out.print("X");	
 		}
-		
-	}
-	/**
-	 * returns the index of a given set of coordinates
-	 * @param i
-	 * @param j
-	 * @return
-	 */
-	private int getIndex(int i, int j) {
-		if (i < 0 || i > mySize || j < 0 || j > mySize) {
-			throw new IndexOutOfBoundsException();
-		}
-		return (i-1)*mySize + (j-1);
-	}
-	
-	/**
-	 * open site (row i, column j) if it is not open already
-	 * @param i = row
-	 * @param j = column
-	 */
-	public void open (int i, int j) {
-		validate(i, j);
-		if (!sites[i][j]) 
-			sites[i][j] = true; // sites[i][j] is now open
-		
-		int index = getIndex(i, j);
-		
-		// if opening top row, connect to virtual top
-		if (i == 0) {
-			myPerc.union(myTop, index);
-			myFull.union(myTop, index);
-		}
-		if (i == mySize) myPerc.union(myBottom, index);
-	}
-	
-	/**
-	 * is site (row i, column j) open?
-	 * @param i = row
-	 * @param j = column
-	 * @return
-	 */
-	public boolean isOpen(int i, int j) {
-		if (i < 0 || j < 0 || i >= mySize || j >= mySize) return false; // handles if array would go out of bounds
-		return sites[i][j];
-	}
-	
-	/**
-	 * is site (row i, column j) full?
-	 * @param i = row
-	 * @param j = column
-	 * @return
-	 */
-	public boolean isFull(int i, int j) { 
-		validate(i, j);
-		// a full site is an open site that can be connected to an open site in the top row via a chain of neighboring (left, right, up, down) sites
-		return myFull.connected(myTop, getIndex(i, j));
-	}
-	
-	/**
-	 * does the system percolate?
-	 * @return
-	 */
-	public boolean percolates() {
-		// see explanation in assignment pdf file to know what percolation means
-		return myPerc.connected(myTop, myBottom);
-	}
-	
-	/** 
-	 * validates a pair of coordinates. I'm aware a function like this in QuickFindUF, but since I might decide
-	 * to change this function later I wanted to make a spare one here, just in case
-	 * @param i
-	 * @param j
-	 */
-	public void validate(int i, int j) {
-		if (i < 0 || i >= mySize) {
-			throw new IllegalArgumentException("open(): index " + i + " is not between 0 and " + (mySize-1));
-		}
-		else if (j < 0 || j >= mySize) {
-			throw new IllegalArgumentException("open(): index " + j + " is not between 0 and " + (mySize-1));
-		}
-	}
-	
-	/**
-	 * prints out the contents of the percolation to determine visually which sites are open
-	 */
-	public void print() {
-		for (int i = 0; i < mySize; i++) {
-			for (int j = 0; j < mySize; j++) {
-				if (sites[i][j]) System.out.print("O");
-				else System.out.print("X");
-				
-			}
-			System.out.print("\n");
-		}
-	}
+            System.out.println();
+        }
+        System.out.println();
+    }  
+    
+    public boolean percolates(){
+        return myPerc.connected(myTop, myBottom);
+    }
 }
-
-	
